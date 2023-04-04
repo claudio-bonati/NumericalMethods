72f356d40bf9476df0b37fa57aeff8fd1baad608
Claudio Bonati <claudio.bonati82@gmail.com>
2023-04-04 21:20:08 +0200

    nvector library added.

---
+++ ModuleA/include/nvector.h
@@ -0,0 +1,40 @@
+#ifndef NVECTOR_H
+
+#define NCOMP 3
+
+typedef struct NVec {
+     double comp[NCOMP];
+} NVec;
+
+//initialize
+void init(NVec *a, double b[NCOMP]);
+
+//initialize to zeros
+void zeros(NVec *a);
+
+//initialize to one in the 0 direction
+void one(NVec *a);
+
+//assignement
+void equal(NVec *a, NVec const * const b);
+
+//times equal
+void timesequal(NVec *a, double d);
+
+//sumequal
+void sumequal(NVec *a, NVec const * const b);
+
+//sum
+void sum(NVec *a, NVec const * const b, NVec const * const c);
+
+//scalar product
+double scalprod(NVec const * const a, NVec const * const b);
+
+//rotate two components by angle
+void rotate2(NVec *a, int i, int j, double phi);
+
+//normalize
+void normalize(NVec *a);
+
+
+#endif
---
+++ ModuleA/lib/nvector.c
@@ -0,0 +1,137 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"../include/nvector.h"
+
+//initialize
+void init(NVec *a, double b[NCOMP])
+  {
+  int i;
+
+  for(i=0; i<NCOMP; i++)
+     {
+     a->comp[i]=b[i];
+     }
+  }
+
+
+//initialize to zeros
+void zeros(NVec *a)
+  {
+  int i;
+
+  for(i=0; i<NCOMP; i++)
+     {
+     a->comp[i]=0.0;
+     }
+  }
+
+
+//initialize to one
+void one(NVec *a)
+  {
+  int i;
+
+  a->comp[0]=0.0;
+  for(i=1; i<NCOMP; i++)
+     {
+     a->comp[i]=0.0;
+     }
+  }
+
+
+//assignement
+void equal(NVec *a, NVec const * const b)
+  {
+  int i;
+
+  for(i=0; i<NCOMP; i++)
+     {
+     a->comp[i]=b->comp[i];
+     }
+  }
+
+
+//times equal
+void timesequal(NVec *a, double d)
+  {
+  int i;
+
+  for(i=0; i<NCOMP; i++)
+     {
+     (a->comp[i])*=d;
+     }
+  }
+
+
+//sumequal
+void sumequal(NVec *a, NVec const * const b)
+  {
+  int i;
+
+  for(i=0; i<NCOMP; i++)
+     {
+     a->comp[i]+=b->comp[i];
+     }
+  }
+
+
+//sum
+void sum(NVec *a, NVec const * const b, NVec const * const c)
+  {
+  int i;
+
+  for(i=0; i<NCOMP; i++)
+     {
+     a->comp[i]=b->comp[i]+c->comp[i];
+     }
+  }
+
+//scalar product
+double scalprod(NVec const * const a, NVec const * const b)
+  {
+  int i;
+  double ris=0.0;
+
+  for(i=0; i<NCOMP; i++)
+     {
+     ris+=(a->comp[i])*(b->comp[i]);
+     }
+
+  return ris;
+  }
+
+
+//rotate two components by angle
+void rotate2(NVec *a, int i, int j, double phi)
+  {
+  double v1, v2;
+
+  v1=a->comp[i];
+  v2=a->comp[j];
+
+  a->comp[i]= v1*cos(phi)+v2*sin(phi);
+  a->comp[j]=-v1*sin(phi)+v2*cos(phi);
+  }
+
+
+//normalize
+void normalize(NVec *a)
+  {
+  int i;
+  double norm=0.0;
+  
+  for(i=0; i<NCOMP; i++)
+     {
+     norm+=(a->comp[i])*(a->comp[i]);
+     }
+  norm=1.0/sqrt(norm);
+ 
+  for(i=0; i<NCOMP; i++)
+     {
+     (a->comp[i])*=norm;
+     }
+  }
+
+
