733ce5df54bbce03b68fadae5b17d4ebff34655c
Claudio Bonati <claudio.bonati82@gmail.com>
2023-04-13 17:01:04 +0200

    computation of <x^4> added in mcmc_analysis1.c

--- ModuleA/src/mcmc_analysis1.c
+++ ModuleA/src/mcmc_analysis1.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     {
     int j, therm, binsize, err;
     long int i, sample, numberofbins;
-    double tmp, x, x2, xb2, x2b2, xtmp, x2tmp;
+    double tmp, x, x2, x4, xb2, x2b2, x4b2, xtmp, x2tmp, x4tmp; // see line 80 for explanation
     char datafile[STRING_LENGTH];
     FILE *fp;
 
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
       fprintf(stdout, "  binsize = size of the bin to be used in binning/blocking\n");
       fprintf(stdout, "  datafile = name of the data file to be analyzed (single column!)\n\n");
       fprintf(stdout, "Output:\n");
-      fprintf(stdout, "  <x> and it error, <x^2> and its error\n");
+      fprintf(stdout, "  <x> and it error, <x^2> and its error, <x^4> and its error\n");
 
       return EXIT_SUCCESS;
       }
@@ -78,16 +78,19 @@ int main(int argc, char **argv)
        }
 
     // intialize average values
-    x=0.0;
-    x2=0.0;
-    xb2=0.0;  // (binned x) squared
-    x2b2=0.0; // (x^2 binned) squared
- 
+    x=0.0;    // for <x>
+    x2=0.0;   // for <x^2>
+    x4=0.0;   // for <x^4>
+    xb2=0.0;  // <(binned x)^2>
+    x2b2=0.0; // <(x^2 binned)^2>
+    x4b2=0.0; // <(x^4 binned)^2>
+
     for(i=0; i<numberofbins; i++)
        {
-       xtmp=0.0;
-       x2tmp=0.0;
- 
+       xtmp=0.0;  // x binned
+       x2tmp=0.0; // x^2 binned
+       x4tmp=0.0; // x^4 binned
+
        for(j=0; j<binsize; j++)
           {
           err=fscanf(fp, "%lf", &tmp);
@@ -99,25 +102,35 @@ int main(int argc, char **argv)
    
           xtmp+=tmp;
           x2tmp+=pow(tmp,2.0);
+          x4tmp+=pow(tmp,4.0);
           }
 
        xtmp/=(double) binsize;
        x2tmp/=(double) binsize;
+       x4tmp/=(double) binsize;
 
        x+=xtmp;
        xb2+=xtmp*xtmp;
 
        x2+=x2tmp;
        x2b2+=x2tmp*x2tmp;
+
+       x4+=x4tmp;
+       x4b2+=x4tmp*x4tmp;
        }
 
     x/=(double)numberofbins;
     xb2/=(double)numberofbins;
+
     x2/=(double)numberofbins;
     x2b2/=(double)numberofbins;
 
+    x4/=(double)numberofbins;
+    x4b2/=(double)numberofbins;
+
     printf("%f %f ", x, sqrt(xb2-x*x)/sqrt((double)numberofbins));
-    printf("%f %f\n", x2, sqrt(x2b2-x2*x2)/sqrt((double)numberofbins));
+    printf("%f %f ", x2, sqrt(x2b2-x2*x2)/sqrt((double)numberofbins));
+    printf("%f %f\n", x4, sqrt(x4b2-x4*x4)/sqrt((double)numberofbins));
 
     return EXIT_SUCCESS;
     }
