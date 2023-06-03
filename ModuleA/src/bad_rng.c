d2664c312477ffdd141e828fb214cbceac9bd361
Claudio Bonati <claudio.bonati82@gmail.com>
2023-06-03 10:40:51 +0200

    sligthly modified bad_rng.c. Option added to print points in d-dim.

--- ModuleA/src/bad_rng.c
+++ ModuleA/src/bad_rng.c
@@ -39,22 +39,22 @@ double randu()
 int main(int argc, char **argv)
     {
     unsigned long int seed;
-    int i, maxiter;
-    double x, x2, xy, xyz;
-    double sigma_x, sigma_x2, sigma_xy, sigma_xyz;
-    double tmp1, tmp2, tmp3;
+    int j, dim;
+    long i, maxiter;
+    double x, x2, sigma_x, tmp;
     char datafile[STRING_LENGTH];
     FILE *fp;
 
-    if(argc != 4)
+    if(argc != 5)
       {
       fprintf(stdout, "How to use this program:\n");
-      fprintf(stdout, "  %s seed sample datafile\n\n", argv[0]);
+      fprintf(stdout, "  %s seed dim sample datafile\n\n", argv[0]);
       fprintf(stdout, "  seed = seed for the random number generator (0 = machine time)\n");
+      fprintf(stdout, "  dim = random points in (0,1)^{dim} are generated\n");
       fprintf(stdout, "  sample = number of points to be generated\n");
       fprintf(stdout, "  datafile = name of the file in which to print three columns of 'sample' random data\n\n");
       fprintf(stdout, "Output:\n");
-      fprintf(stdout, "  some statistical test\n");
+      fprintf(stdout, "  some statistical test\n\n");
 
       return EXIT_SUCCESS;
       }
@@ -63,16 +63,17 @@ int main(int argc, char **argv)
       // read input values 
 
       seed=(unsigned long int)atoi(argv[1]);
-      maxiter=atoi(argv[2]);
+      dim=atoi(argv[2]);
+      maxiter=atol(argv[3]);
 
-      if(strlen(argv[3]) >= STRING_LENGTH)
+      if(strlen(argv[4]) >= STRING_LENGTH)
         {
         fprintf(stderr, "File name too long. Increse STRING_LENGTH or shorten the name (%s, %d)\n", __FILE__, __LINE__);
         return EXIT_FAILURE;
         }
       else
         {
-        strcpy(datafile, argv[3]);
+        strcpy(datafile, argv[4]);
         }
       }
 
@@ -99,59 +100,35 @@ int main(int argc, char **argv)
       return EXIT_FAILURE;
       }
 
-    // intialize average values
+    // initialize counters
     x=0.0;
     x2=0.0;
-    xy=0.0;
-    xyz=0.0;
 
     // loop on iterations
     for(i=0; i<maxiter; i++)
        {
-       tmp1=randu();
-       tmp2=randu();
-       tmp3=randu();
-
-       fprintf(fp, "%f %f %f\n", tmp1, tmp2, tmp3);
-
-       x+=tmp1;
-       x2+=tmp1*tmp1;
-       xy+=tmp1*tmp2;
-       xyz+=tmp1*tmp2*tmp3;
+       for(j=0; j<dim; j++)
+          {
+          tmp=randu();
+          x+=tmp;
+          x2+=tmp;
+          fprintf(fp, "%f ", tmp);
+          }
+       fprintf(fp,"\n");
        }
 
     // close data file
     fclose(fp);
 
     // normalize
-    x/=(double) (maxiter);
-    x2/=(double) (maxiter);
-    xy/=(double) (maxiter);
-    xyz/=(double) (maxiter);
+    x/=((double) (maxiter)*(double) dim);
+    x2/=((double) (maxiter)*(double) dim);
 
     //<x> = 1/2
-    sigma_x=sqrt(1./3. - 1./4.)/sqrt(maxiter); // theoretica std
+    sigma_x=sqrt(1./3. - 1./4.)/sqrt((double) maxiter); // theoretical std
     printf("<x>-exact=%f ; ", x-1./2);
     printf("th_sigma=%.6f ; ", sigma_x);
     printf("(<x>-exact)/th_sigma=%f\n", (x-1./2.)/sigma_x);
-
-    //<x^2> = 1/3
-    sigma_x2=sqrt(1./5. - 1./9.)/sqrt(maxiter); // theoretical std
-    printf("<x2>-exact=%.6f ; ", x2-1./3.);
-    printf("th_sigma=%f ; ", sigma_x2);
-    printf("(<x2>-exact)/th_sigma=%f\n", (x2-1./3.)/sigma_x2);
-
-    //<xy> = 1/4
-    sigma_xy=sqrt(1./9. - 1./16.)/sqrt(maxiter);  // theoretical std
-    printf("<xy>-exact=%.6f ; ", xy-1./4.);
-    printf("th_sigma=%f ; ", sigma_xy);
-    printf("(<xy>-exact)/th_sigma=%f\n", (xy-1./4.)/sigma_xy);
-
-    //<xyz> = 1/8
-    sigma_xyz=sqrt(1./27. - 1./64.)/sqrt(maxiter); // theoretical std
-    printf("<xyz>-exact=%.6f ; ", xyz - 1./8.);
-    printf("th_sigma=%f ; ", sigma_xyz);
-    printf("(<xyz>-exact)/th_sigma=%f\n", (xyz-1./8.)/sigma_xyz);
 
     return EXIT_SUCCESS;
     }
