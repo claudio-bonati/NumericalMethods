150b9fcdeeaf30f5365b043f8ee0d45a32bad205
Claudio Bonati <claudio.bonati82@gmail.com>
2023-08-05 14:25:53 +0200

    minor changes.

---
+++ ModuleB/src/harm_osc_th.c
@@ -0,0 +1,247 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+#include"../include/boxmuller.h"
+#include"../include/random.h"
+
+#define STRING_LENGTH 50
+
+//#define METRO // if this macro is defined Metropolis is uded, otherwise heatbath
+
+// average position
+double calc_x(double const * const restrict lattice, long int Nt)
+  {
+  long int r;
+  double ris;
+
+  ris=0.0;
+  for(r=0; r<Nt; r++)
+     {
+     ris+=lattice[r];
+     }
+  
+  return ris/(double)Nt;
+  }
+
+
+// average position square
+double calc_x2(double const * const restrict lattice, long int Nt)
+  {
+  long int r;
+  double ris;
+
+  ris=0.0;
+  for(r=0; r<Nt; r++)
+     {
+     ris+=lattice[r]*lattice[r];
+     }
+  
+  return ris/(double)Nt;
+  }
+
+
+// average kinetic energy (naive!)
+double calc_Knaive(double const * const restrict lattice, 
+                   long int const * const restrict nnp, 
+                   long int Nt, 
+                   double eta)
+  {
+  long int r;
+  double ris, aux;
+
+  ris=0.0;
+  for(r=0; r<Nt; r++)
+     {
+     aux=lattice[nnp[r]]-lattice[r];
+     ris+=aux*aux/eta/eta;
+     }
+  
+  return ris/(double)Nt/2.0;
+  }
+
+
+// Metropolis update, return 1
+int metropolis(double * restrict lattice, long int r, double nnsum, double eta)
+  {
+  const double delta=10.0*sqrt(eta);
+  double trial, Eold, Enew;
+
+  Eold=lattice[r]*lattice[r]*(eta/2.0+1./eta)-lattice[r]*nnsum/eta;
+  trial=lattice[r]+delta*(1.0-2.0*myrand());
+  Enew=trial*trial*(eta/2.0+1./eta)-trial*nnsum/eta;
+
+  if(Enew<Eold)
+    {
+    lattice[r]=trial;
+    return 1;
+    }
+  else if(myrand()<exp(-(Enew-Eold)) )
+         {
+         lattice[r]=trial;
+         return 1;
+         }
+
+  return 0; 
+  }
+
+
+// Heatbath update, return 1
+int heatbath(double * restrict lattice, long int r, double nnsum, double eta)
+  {
+  const double std=1.0/sqrt(eta+2.0/eta);
+  const double average=nnsum/eta/(eta+2.0/eta);
+ 
+  lattice[r]=average+std*gauss1();
+
+  return 1; 
+  }
+
+
+// main
+int main(int argc, char **argv)
+    {
+    double *lattice;
+    long int Nt, r, sample, iter; 
+    long int *nnp, *nnm;
+    double simbeta, eta, nnsum;
+    long int acc;
+    double x, x2, Knaive; 
+  
+    char datafile[STRING_LENGTH];
+    FILE *fp;
+
+    const int measevery=10;
+
+    const unsigned long int seed1=(unsigned long int) time(NULL);
+    const unsigned long int seed2=seed1+127;
+
+    if(argc != 5)
+      {
+      fprintf(stdout, "How to use this program:\n");
+      fprintf(stdout, "  %s simbeta Nt sample datafile\n\n", argv[0]);
+      fprintf(stdout, "  simbeta = hbar*omega/(k_B T)\n");
+      fprintf(stdout, "  Nt = number of temporal steps\n");
+      fprintf(stdout, "  sample = number of drawn to be extracted\n");
+      fprintf(stdout, "  datafile = name of the file on which to write the data\n\n");
+      fprintf(stdout, "Output:\n");
+      fprintf(stdout, "  TO BE MODIFIED\n");
+
+      return EXIT_SUCCESS;
+      }
+    else
+      {  
+      // read input values 
+      simbeta=atof(argv[1]);
+      Nt=atol(argv[2]);
+      sample=atol(argv[3]);
+
+      if(strlen(argv[4]) >= STRING_LENGTH)
+        {
+        fprintf(stderr, "File name too long. Increse STRING_LENGTH or shorten the name (%s, %d)\n", __FILE__, __LINE__);
+        return EXIT_FAILURE;
+        }
+      else
+        {
+        strcpy(datafile, argv[4]);
+        }
+      }
+
+    if(Nt<=1)
+      {
+      fprintf(stderr, "'Nt' must be larger than 1\n");
+      return EXIT_FAILURE;
+      }
+
+    if(sample<=0)
+      {
+      fprintf(stderr, "'sample' must be positive\n");
+      return EXIT_FAILURE;
+      }
+
+    // definition of eta
+    eta=simbeta/(double)Nt;
+
+    // initialize random number generator
+    myrand_init(seed1, seed2);
+
+    // allocate the lattice and next neighbors
+    lattice=(double *)malloc((unsigned long int)(Nt)*sizeof(double));
+    if(lattice == NULL)
+      {
+      fprintf(stderr, "allocation problem at (%s, %d)\n", __FILE__, __LINE__);
+      return EXIT_FAILURE;
+      }
+    nnp=(long int *)malloc((unsigned long int)(Nt)*sizeof(long int));
+    if(nnp == NULL){
+      fprintf(stderr, "allocation problem at (%s, %d)\n", __FILE__, __LINE__);
+      return EXIT_FAILURE;
+      }
+    nnm=(long int *)malloc((unsigned long int)(Nt)*sizeof(long int));
+    if(nnm == NULL){
+      fprintf(stderr, "allocation problem at (%s, %d)\n", __FILE__, __LINE__);
+      return EXIT_FAILURE;
+      }
+
+    // initialize nnp and nnm for periodic b.c.
+    for(r=0; r<Nt; r++)
+       {
+       nnp[r]=r+1;
+       nnm[r]=r-1;
+       }
+    nnp[Nt-1]=0;
+    nnm[0]=Nt-1;
+
+    // initialize lattice 
+    for(r=0; r<Nt; r++)
+       {
+       lattice[r]=0.0;
+       }
+
+    // open data file
+    fp=fopen(datafile, "w");
+    if(fp==NULL)
+      {
+      fprintf(stderr, "Error in opening the file %s (%s, %d)\n", datafile, __FILE__, __LINE__);
+      return EXIT_FAILURE;
+      }
+
+    acc=0;
+    for(iter=0; iter<sample; iter++)
+       {
+       for(r=0; r<Nt; r++)
+          {
+          nnsum=lattice[nnp[r]]+lattice[nnm[r]];
+
+          #ifdef METRO
+            acc += metropolis(lattice, r, nnsum, eta);
+          #else
+            acc += heatbath(lattice, r, nnsum, eta);
+          #endif
+          }
+
+       if(iter%measevery==0)
+         {
+         x=calc_x(lattice, Nt);   
+         x2=calc_x2(lattice, Nt); 
+         Knaive=calc_Knaive(lattice, nnp, Nt, eta);
+  
+         fprintf(fp, "%f %f %f\n", x, x2, Knaive);
+         }
+       }
+
+    printf("Acceptance rate %f\n", (double)acc / (double)sample / (double) Nt);
+
+    // close datafile
+    fclose(fp);
+
+    free(lattice);
+    free(nnp);
+    free(nnm);
+
+    return EXIT_SUCCESS;
+    }
+
+
---
+++ ModuleB/src/harm_osc_th_analysis.c
@@ -0,0 +1,192 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include"../include/read_data.h"
+
+#define STRING_LENGTH 50
+
+// REMEMBER: the data file is a two column file, each raw corresponding to measures taken on the same configuraton
+// first column = x (averaged on Nt)
+// second column = x^2 (averaged on Nt)
+// thir columns = K (averaged on Nt)
+
+// compute the jacknife samples of <x>, <x^2>, <Knaive>, <Hnaive>, <H>
+void computejack(double * restrict datajack, 
+                 double const * const restrict data, 
+                 long int numberofbins, 
+                 int binsize,
+                 double eta)
+  {
+  long int i, r;
+  int j;
+  const long int sampleeff=numberofbins*(long int) binsize;
+  double xtot, x2tot, Ktot;
+  double x, x2, K;
+
+  xtot=0.0;
+  x2tot=0.0;
+  Ktot=0.0;
+
+  for(i=0; i<sampleeff; i++)
+     {
+     xtot +=data[3*i+0];
+     x2tot+=data[3*i+1];
+     Ktot +=data[3*i+2];
+     }
+
+  for(i=0; i<numberofbins; i++)
+     {
+     x=xtot;
+     x2=x2tot;
+     K=Ktot;
+
+     for(j=0; j<binsize; j++)
+        {
+        r=i*binsize+j;
+
+        x -=data[3*r+0];
+        x2-=data[3*r+1];
+        K -=data[3*r+2];
+        }
+
+     x/=(double)((numberofbins-1)*binsize); 
+     x2/=(double)((numberofbins-1)*binsize);
+     K/=(double)((numberofbins-1)*binsize);
+  
+     datajack[4*i+0]=x;
+     datajack[4*i+1]=x2;
+     datajack[4*i+2]=K;
+     datajack[4*i+3]=0.5*x2 -K +0.5/eta;
+     }
+  }
+
+
+// main
+int main(int argc, char **argv)
+    {
+    int therm, binsize, j;
+    long int sample, numberofbins, sampleeff, i, Nt;
+    double simbeta, eta;
+    double *data, *datajack, ris[4], err[4];   // 4 becuse there are 4 observables
+    char datafile[STRING_LENGTH];
+
+    if(argc != 6)
+      {
+      fprintf(stdout, "How to use this program:\n");
+      fprintf(stdout, "  %s therm binsize datafile simbeta Nt\n\n", argv[0]);
+      fprintf(stdout, "  therm = number of lines to be discarded as thermalization\n");
+      fprintf(stdout, "  binsize = size of the bin to be used in binning/blocking\n");
+      fprintf(stdout, "  datafile = name of the data file to be analyzed\n");
+      fprintf(stdout, "  simbeta = hbar*omega/(k_B T)\n");
+      fprintf(stdout, "  Nt = number of temporal steps\n");
+
+      fprintf(stdout, "  eta = value of eta used in the simulation\n\n");
+      fprintf(stdout, "Output:\n");
+      fprintf(stdout, "  <x>, err, <x^2>, err, <Knaive>, err, <H>, err\n");
+      fprintf(stdout, "  computed using binning and jackknife\n");
+
+      return EXIT_SUCCESS;
+      }
+    else
+      {
+      // read input
+      therm=atoi(argv[1]);
+      binsize=atoi(argv[2]);
+
+      if(strlen(argv[3]) >= STRING_LENGTH)
+        {
+        fprintf(stderr, "File name too long. Increse STRING_LENGTH or shorten the name (%s, %d)\n", __FILE__, __LINE__);
+        return EXIT_FAILURE;
+        }
+      else
+        {
+        strcpy(datafile, argv[3]);
+        }
+      }
+    simbeta=atof(argv[4]);
+    Nt=atol(argv[5]);
+
+    if(Nt<=1)
+      {
+      fprintf(stderr, "'Nt' must be larger than 1\n");
+      return EXIT_FAILURE;
+      }
+
+    // definition of eta
+    eta=simbeta/(double) Nt;
+
+    if(binsize<=0)
+      {
+      fprintf(stderr, "'binsize' must be positive\n");
+      return EXIT_FAILURE;
+      }
+
+    // determine the length of the file
+    sample=linecounter_mc(datafile, 3);
+
+    // initialize numberofbins and sampleeff
+    numberofbins=(sample-therm)/binsize;
+    sampleeff=numberofbins*binsize;
+
+    // allocate data arrays
+    data=(double *)malloc((unsigned long int)(3*sampleeff)*sizeof(double)); // 3 columns!
+    if(data==NULL)
+      {
+      fprintf(stderr, "Allocation problem at (%s, %d)\n", __FILE__, __LINE__);
+      return EXIT_FAILURE;
+      }
+
+    // allocate jackknife samples
+    datajack=(double *)malloc((unsigned long int)(4*numberofbins)*sizeof(double)); // 4 because there are 4 observables
+    if(datajack==NULL)
+      {
+      fprintf(stderr, "Allocation problem at (%s, %d)\n", __FILE__, __LINE__);
+      return EXIT_FAILURE;
+      }
+
+    // initialize data
+    readdata_mc(datafile, therm, sampleeff, data, 3);
+
+    // compute jackknife resamplings
+    computejack(datajack, data, numberofbins, binsize, eta);
+
+    // compute average
+    for(j=0; j<4; j++)
+       {
+       ris[j]=0.0;
+       for(i=0; i<numberofbins; i++)
+          {
+          ris[j]+=datajack[4*i+j];
+          }
+       ris[j]/=(double)numberofbins;
+       }
+
+    // compute error
+    for(j=0; j<4; j++)
+       {
+       err[j]=0.0;
+       for(i=0; i<numberofbins; i++)
+          {
+          err[j]+=pow(ris[j]-datajack[4*i+j], 2.0);
+          }
+       // this corrects for a factor that is irrelevant but we leave it just for clarity
+       err[j]*=(double)(numberofbins-1);
+       err[j]/=(double)numberofbins;
+       err[j]=sqrt(err[j]);
+       }
+
+    // free data arrays
+    free(data);
+    free(datajack);
+
+    for(j=0; j<4; j++)
+       {
+       printf("%f %f ", ris[j], err[j]);
+       }
+    printf("\n");
+
+    return EXIT_SUCCESS;
+    }
+
