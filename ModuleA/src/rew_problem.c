5b77e7efc778231a3c0c6bb950be5d8d50304066
Claudio Bonati <claudio.bonati82@gmail.com>
2023-09-25 19:09:30 +0200

    ModuleA/src/rew_problem.c simplified.

--- ModuleA/src/rew_problem.c
+++ ModuleA/src/rew_problem.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
     {
     long int i, sample;
-    double mu, tmp1, tmp2, aux, x, x2, Z, Z2, sigmax, sigmaZ;
+    double mu, tmp1, tmp2, aux, x, x2, sigmax;
     double ris, err;
     const unsigned long int seed1=(unsigned long int) time(NULL);
     const unsigned long int seed2=seed1+127;
@@ -46,20 +46,15 @@ int main(int argc, char **argv)
     // initialize average values
     x=0.0;
     x2=0.0;
-    Z=0.0;
-    Z2=0.0;
 
     // average values are computed by using
     // \int x e^{-(x-mu)^2/2} = \int x e^{-(x-mu)^2/2}e^{x^2/2}e^{-x^2/2} 
     // = (\int e^{-x^2/2}) * <x e^{-(x-mu)^2/2}e^{x^2/2}>_{N(0,1)}
     //
-    // \int e^{-(x-mu)^2/2} = \int e^{-(x-mu)^2/2}e^{x^2/2}e^{-x^2/2} 
-    // = (\int e^{-x^2/2}) * <e^{-(x-mu)^2/2}e^{x^2/2}>_{N(0,1)}
-    //
     // hence <x>_{N(mu,1)}=
-    // <x e^{-(x-mu)^2/2}e^{x^2/2}>_{N(0,1)} / <e^{-(x-mu)^2/2}e^{x^2/2}>_{N(0,1)}
+    // <x e^{-(x-mu)^2/2}e^{x^2/2}>_{N(0,1)}
+    // since the gaussian integral does not depend on mu
 
-    // numerator 
     for(i=0; i<sample/2; i++)
        {
        gauss2(&tmp1, &tmp2);
@@ -85,36 +80,8 @@ int main(int argc, char **argv)
     x2/=(double) sample;
     sigmax=sqrt(x2-x*x)/sqrt((double) sample);
 
-    // denominator
-    for(i=0; i<sample/2; i++)
-       {
-       gauss2(&tmp1, &tmp2);
-   
-       aux=exp(-(tmp1-mu)*(tmp1-mu)*0.5+tmp1*tmp1*0.5);
-       Z+=aux;
-       Z2+=aux*aux;
-
-       aux=exp(-(tmp2-mu)*(tmp2-mu)*0.5+tmp2*tmp2*0.5);
-       Z+=aux;
-       Z2+=aux*aux;
-       }
-    if(sample % 2 == 1)
-      {
-      tmp1+=gauss1();
-
-      aux=exp(-(tmp1-mu)*(tmp1-mu)*0.5+tmp1*tmp1*0.5);
-      Z+=aux;
-      Z2+=aux*aux;
-      } 
-
-    Z/=(double) sample;
-    Z2/=(double) sample;
-    sigmaZ=sqrt(Z2-Z*Z)/sqrt((double) sample);
- 
-    ris=x/Z;
-    err=ris*sqrt( pow(sigmax/x,2.0)+pow(sigmaZ/Z,2.0));
-    // we can use error propagation since we used two different random samples
-
+    ris=x;
+    err=sigmax;
     printf("<x>=%lf %lf\n", ris, err);
 
     return EXIT_SUCCESS;
