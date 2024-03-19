e9e03a46400bf66ffcaa4e18a9f6634b506bce9c
Claudio Bonati <claudio.bonati82@gmail.com>
2024-03-19 21:56:17 +0100

    trivial change (spaces cancelled).

--- ModuleC/src/u1_conf.c
+++ ModuleC/src/u1_conf.c
@@ -10,7 +10,7 @@
 
 // when using C99 M_PI is not defined in math.h header!
 #ifndef M_PI
-#  define M_PI  3.141592653589793238462643383279502884
+  #define M_PI  3.141592653589793238462643383279502884
 #endif
 
 #define MIN(a,b) (((a)<(b))?(a):(b))  // min of two numbers
