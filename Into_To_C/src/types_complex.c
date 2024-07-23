d5ccce0f46f574d129b6d54bbcd029edcbd90135
Claudio Bonati <claudio.bonati82@gmail.com>
2024-07-23 19:27:40 +0200

    typo corrected in comments.

--- Into_To_C/src/types_complex.c
+++ Into_To_C/src/types_complex.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 
 
-// this code needs -std=c99 to be added to the string compilation
+// this code needs -std=c99 to be added to the compilation string
 // to properly compile, although it is likely that some compiler
 // can understand this also without explicitly specifying it 
 
