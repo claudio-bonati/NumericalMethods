bf41b7f66badd9d6bfdce45213e8945b991d5053
Claudio Bonati <claudio.bonati82@gmail.com>
2024-10-01 21:18:34 +0200

    comments added on multifile compilation.

--- Into_To_C/src/integrator2.c
+++ Into_To_C/src/integrator2.c
@@ -20,25 +20,25 @@ int main(void)
 
     printf("Rectangle method\n");
 
-    ris=int_rel_rec(0.0, 1.0, &test, prec, &err);
+    ris=int_rel_rec(0.0, 1.0, &test, prec, &err); // this is declared in int_lib.h and defined in int_lib.c
     if(err==0)
       {
       printf("%.14lf\n", ris);
       }
 
-    ris=int_abs_rec(0.0, 1.0, &test, prec, &err);
+    ris=int_abs_rec(0.0, 1.0, &test, prec, &err);  // this is declared in int_lib.h and defined in int_lib.c
     if(err==0)
       {
       printf("%.14lf\n", ris);
       }
 
-    ris=int_rel_rec(0.0, 2.0*pi, &sin, prec, &err);
+    ris=int_rel_rec(0.0, 2.0*pi, &sin, prec, &err);  // this is declared in int_lib.h and defined in int_lib.c
     if(err==0)
       {
       printf("%.14lf\n", ris);
       }
 
-    ris=int_abs_rec(0.0, 2.0*pi, &sin, prec, &err);
+    ris=int_abs_rec(0.0, 2.0*pi, &sin, prec, &err); // this is declared in int_lib.h and defined in int_lib.c
     if(err==0)
       {
       printf("%.14lf\n", ris);
